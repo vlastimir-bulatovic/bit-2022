49d2761407f46f89ae8cf08dc6be3b77e0fc2a28
vlastimir-bulatovic <bulatovic.vlastimir98@gmail.com>
2022-11-01 14:23:53 +0100

    Updated

---
+++ pp/week4/day2/exercise8.js
@@ -0,0 +1,45 @@
+// 6.
+
+// function errorCallback(){
+//     return "Your password is invalid";
+// }
+
+// function successCallback() {
+//     return "Your password is cool!";
+// }
+
+// function passwordChecker(pass, error, success) {
+
+//     if(pass.length < 6) return error;
+
+//     for(var i =0; i < pass.length; i++) {
+
+//         if(parseInt(pass[i]) == pass[i]) return success;
+
+//     }
+
+//     return error;
+// }
+
+// var b = passwordChecker("bababa1", errorCallback, successCallback);
+// console.log(b())
+
+// 7.
+
+// function conditionCallback(el) {
+//     return el % 2 === 0 ? true : false ;
+// }
+
+// function filterArray(arr, condition) {
+//     var newArray = [];
+
+//     for(var i =0; i < arr.length; i++) {
+//         if(condition(arr[i])) {
+//             newArray[newArray.length] = arr[i];
+//         }
+//     }
+
+//     return newArray;
+// }
+
+// console.log(filterArray([1, 2, 3, 577, 43, 2, 0], conditionCallback));
