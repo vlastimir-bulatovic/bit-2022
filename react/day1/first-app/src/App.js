1539afc3bbb7fe0789a413f7e95b9f87109d355c
vlastimir-bulatovic <bulatovic.vlastimir98@gmail.com>
2022-11-23 17:58:03 +0100

    Updated

--- react/day1/first-app/src/App.js
+++ react/day1/first-app/src/App.js
@@ -1,5 +1,17 @@
+import "./app.css";
+
 function App() {
-	return <div className="App">Hello from the other side</div>;
+	return (
+		// empty fragment
+		<>
+			<div className="App">
+				Hello from the other side
+				<h1>cao cao</h1>
+				<p>lorem lorem</p>
+			</div>
+		</>
+	);
 }
 
-export default App;
+export default App
+;
